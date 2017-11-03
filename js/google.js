57e2c7cb36d93ee3faa4bb2c9185e76201b196d6
Mykola Dekhtiarenko <nnick1212@gmail.com>
2017-11-03 18:53:44 +0200

    added authorization via google

---
+++ js/google.js
@@ -0,0 +1,30 @@
+/**
+ * Created by mykola.dekhtiarenko on 11/2/17.
+ */
+$(document).ready(function () {
+
+    gapi.load('auth2', function () {
+        // Retrieve the singleton for the GoogleAuth library and set up the client.
+        auth2 = gapi.auth2.init({
+            client_id: '25349712151-aeh42llej5ehnsimvsjk5g4a3mkjrkkq.apps.googleusercontent.com',
+            cookiepolicy: 'single_host_origin',
+            // Request scopes in addition to 'profile' and 'email'
+            //scope: 'additional_scope'
+        });
+        alert("loading.....");
+        attachSignin(document.getElementById('googleLoginBtn'));
+    });
+
+
+    function attachSignin(element) {
+        console.log(element.id);
+        auth2.attachClickHandler(element, {},
+            function (googleUser) {
+                document.getElementById('googleLoginBtn').value = "Continue as " +
+                    googleUser.getBasicProfile().getName();
+            }, function (error) {
+                alert(JSON.stringify(error, undefined, 2));
+            });
+    }
+
+});
